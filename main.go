6cb731345f732777c693f2bab27bab5dd40b8b18
wildeKaiserin <friedri5@hm.edu>
2019-04-22 16:55:51 +0200

    insert methods using local actors

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
-import(
-	"fmt"
-	"github.com/AsynkronIT/protoactor-go/actor"
+import (
 	"github.com/AsynkronIT/goconsole"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"./tree"
 )
 
-
-type hello struct{ Who string }
-
-type helloActor struct{}
-
-func (state *helloActor) Receive(context actor.Context) {
-	switch msg := context.Message().(type) {
-case *hello:
-fmt.Printf("Hello %v\n", msg.Who)
-}
-}
 func main() {
 	context := actor.EmptyRootContext
 	props := actor.PropsFromProducer(func() actor.Actor {
-		return &helloActor{}
+		return &tree.NodeActor{nil, -1, nil, nil, 4}
 	})
 	pid := context.Spawn(props)
-	context.Send(pid, &hello{Who: "Roger"})
+	context.Send(pid, &tree.Insert{5, "five"})
+	context.Send(pid, &tree.Insert{7, "seven"})
+	context.Send(pid, &tree.Insert{9, "nine"})
+	context.Send(pid, &tree.Insert{4, "four"})
+	context.Send(pid, &tree.Insert{6, "six"})
+	context.Send(pid, &tree.Insert{8, "eight"})
+	context.Send(pid, &tree.ShowTree{})
+
 	console.ReadLine()
 }
--- tree/node.go
+++ tree/node.go
@@ -1,1 +1,124 @@
 package tree
+
+import (
+	"fmt"
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"sort"
+)
+
+type Insert struct {
+	Key   int
+	Value string
+}
+
+type InsertMap struct {
+	inserts map[int]string
+}
+
+type delete struct {
+	Key   int
+	Value string
+}
+
+type search struct {
+	Key   int
+	Value string
+}
+
+type traverse struct {
+	Key   int
+	Value string
+}
+
+type ShowTree struct {
+}
+
+type NodeActor struct {
+	Left      *actor.PID
+	LeftMax   int
+	Right     *actor.PID
+	Leaves    map[int]string
+	MaxLeaves int
+}
+
+func (state *NodeActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *Insert:
+		if state.Left != nil {
+			if msg.Key > state.LeftMax {
+				// msg an rechten Node, dass wert einfuegen
+				context.Send(state.Right, &Insert{msg.Key, msg.Value})
+				fmt.Printf("current left baum \n")
+			} else {
+				// msg an linken teilbaum dass er sich drum kuemmert
+				fmt.Printf("muss in linken teilbaum \n")
+				context.Send(state.Left, &Insert{msg.Key, msg.Value})
+			}
+		} else {
+			if state.Leaves == nil {
+				state.Leaves = make(map[int]string)
+			}
+			// pruefen ob map schon voll
+			if len(state.Leaves) == state.MaxLeaves {
+				// split
+				props := actor.PropsFromProducer(func() actor.Actor {
+					return &NodeActor{nil, -1, nil, nil, 4}
+				})
+				state.Left = context.Spawn(props)
+				state.Right = context.Spawn(props)
+				state.Leaves[msg.Key] = msg.Value
+				fmt.Printf("leaves %v", state.Leaves)
+				leftMap, rightMap, leftmaximum := split(state.Leaves)
+				state.LeftMax = leftmaximum
+				context.Send(state.Left, &InsertMap{leftMap})
+				context.Send(state.Right, &InsertMap{rightMap})
+				state.Leaves = nil
+			} else {
+				state.Leaves[msg.Key] = msg.Value
+			}
+		}
+		fmt.Printf("Current map leaves: %v \n", state.Leaves)
+	case *InsertMap:
+		state.Leaves = msg.inserts
+		fmt.Printf("insert map \n")
+	case *delete:
+		fmt.Printf("Hello, I will kill you now!")
+	case *search:
+		fmt.Printf("like sherlock holmes")
+	case *traverse:
+		fmt.Printf("go through")
+	case *ShowTree:
+		if state.Left != nil {
+			context.Send(state.Left, &ShowTree{})
+			context.Send(state.Right, &ShowTree{})
+		} else {
+			fmt.Printf("my map: %v \n", state.Leaves)
+		}
+	}
+}
+
+func sortMap(m map[int]string) []int {
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	fmt.Printf("sort %v \n", keys)
+	return keys
+}
+
+func split(m map[int]string) (leftMap map[int]string, rightMap map[int]string, leftMax int) {
+	sortedKeys := sortMap(m)
+	lengthMap := len(m)/2
+	leftMap = make(map[int]string)
+	rightMap = make(map[int]string)
+
+	for i := 0; i <= lengthMap; i++ {
+		leftMap[sortedKeys[i]] = m[sortedKeys[i]]
+	}
+	for i := lengthMap+1; i < len(m); i++ {
+		rightMap[sortedKeys[i]] = m[sortedKeys[i]]
+	}
+	fmt.Printf("split left %v right %v leftmax %v \n", leftMap, rightMap, sortedKeys[lengthMap])
+	return leftMap, rightMap, sortedKeys[lengthMap]
+}
